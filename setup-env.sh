49996e15c2874b244987c49484d7c8c2b442b25a
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-07-21 23:05:58 -0300

    adding setup-env bash

---
+++ setup-env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -------------------------------------------------
+echo JAVA 
+echo -------------------------------------------------
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install openjdk-8-jdk wget unzip
+echo -------------------------------------------------
+echo GRADLE
+echo -------------------------------------------------
+wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
+sudo mkdir /opt/gradle
+sudo unzip -d /opt/gradle gradle-3.4.1-bin.zip
+export PATH=$PATH:/opt/gradle/gradle-3.4.1/bin
+echo -------------------------------------------------
+echo DOCKER
+echo -------------------------------------------------
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
