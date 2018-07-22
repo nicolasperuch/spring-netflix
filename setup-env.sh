33ac135f1a09b56657c6c6cf7856b5ce8eb89b82
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-07-22 00:02:32 -0300

    Fixing script

--- setup-env.sh
+++ setup-env.sh
@@ -9,10 +9,10 @@ sudo apt-get -y install openjdk-8-jdk wget unzip
 echo -------------------------------------------------
 echo GRADLE
 echo -------------------------------------------------
-wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
+wget https://services.gradle.org/distributions/gradle-4.9-bin.zip
 sudo mkdir /opt/gradle
-sudo unzip -d /opt/gradle gradle-3.4.1-bin.zip
-export PATH=$PATH:/opt/gradle/gradle-3.4.1/bin
+sudo unzip -d /opt/gradle gradle-4.9-bin.zip
+export PATH=$PATH:/opt/gradle/gradle-4.9/bin
 echo -------------------------------------------------
 echo DOCKER
 echo -------------------------------------------------
